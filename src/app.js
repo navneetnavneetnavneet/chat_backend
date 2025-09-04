044c999eb37ecf748b3432e5298695b69acb5f96
Navneet Singh Solanki <navneetsinghsolanki2580@gmail.com>
2025-09-04 16:38:33 +0530

    done

--- src/app.js
+++ src/app.js
@@ -73,6 +73,12 @@ app.use("/api/chats/", chatRoutes);
 app.use("/api/messages/", messageRoutes);
 app.use("/api/status/", statusRoutes);
 
+app.use(express.static(path.join(__dirname, "../public")));
+
+app.get("*name", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/index.html"))
+})
+
 // error-handling
 app.all("*", (req, res, next) => {
   return next(new ErrorHandler(`Requested URL Not Found ${req.url}`, 404));
